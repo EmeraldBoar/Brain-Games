04ba75da481e0fc577e090373177b78a1ba57125
EmeraldBoar <kozlovkv174@gmail.com>
2020-11-12 19:09:15 +0300

    Adds a default parameter

--- src/utils/random.js
+++ src/utils/random.js
@@ -1,3 +1,3 @@
 /* eslint-disable max-len */
 /* eslint-disable import/prefer-default-export */
-export const getRandomNumber = (min, max = 100) => Math.floor(Math.random() * (max - min + 1)) + min;
+export const getRandomNumber = (min = 1, max = 100) => Math.floor(Math.random() * (max - min + 1)) + min;
